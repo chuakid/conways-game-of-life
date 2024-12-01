f34a42df5b481c1b6214c9be91dbc03c01d82eea
Kelvin <chuakid@gmail.com>
2024-12-02 00:01:38 +0800

    direct edit

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,10 @@ import {useEffect, useState} from 'react'
 import styles from "./styles.module.scss"
 import {computeNext, createEmptyGrid} from "./utils.ts";
 
-const SIZE = 20
-const STARTING_ARRAY: boolean[][] = createEmptyGrid(SIZE, SIZE)
-STARTING_ARRAY[5][6] = true
-STARTING_ARRAY[6][6] = true
-STARTING_ARRAY[7][6] = true
 
 function App() {
-    const [grid, setGrid] = useState<boolean[][]>(STARTING_ARRAY)
+    const [grid, setGrid] = useState<boolean[][]>(createEmptyGrid(20, 20))
     const [start, setStart] = useState(false)
-    const [edit, setEdit] = useState(false)
     useEffect(() => {
         const timer = start ? setInterval(() => {
             if (start) {
@@ -23,25 +17,18 @@ function App() {
 
     return (
         <main>
-            <button onClick={() => {
-                setStart(!start)
-                setEdit(false)
-
-            }}>{start ? "Stop" : "Start"}</button>
-            <button onClick={() => {
-                setStart(false)
-                setEdit(!edit)
-            }}>{edit ? "Stop editing" : "Start editing"}</button>
+            <button onClick={() => setStart(!start)
+            }>{start ? "Stop" : "Start"}</button>
             {grid.map((row, i) =>
                 <div key={`${i}`} className={styles.row}>
                     {row.map((col, j) => <div key={`${i}-${j}`} className={styles.box}
                                               onClick={() => {
-                                                  if (edit)
-                                                      setGrid(grid => {
-                                                          const newGrid = structuredClone(grid)
-                                                          newGrid[i][j] = !grid[i][j];
-                                                          return newGrid
-                                                      })
+                                                  setStart(false)
+                                                  setGrid(grid => {
+                                                      const newGrid = structuredClone(grid)
+                                                      newGrid[i][j] = !grid[i][j];
+                                                      return newGrid
+                                                  })
                                               }}
                                               style={{background: col ? "green" : "white"}}/>)}
                 </div>)
